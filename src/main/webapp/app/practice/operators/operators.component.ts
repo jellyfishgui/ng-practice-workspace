85aeaa98e79204b646d8c3d743c2a81bbf17f358
jellyfishgui <marcosgui2048@gmail.com>
2023-11-09 23:14:34 +0800

    feat: practice switch all operator

--- src/main/webapp/app/practice/operators/operators.component.ts
+++ src/main/webapp/app/practice/operators/operators.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject, concat, delay, fromEvent, interval, merge, of, range, take } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, mergeAll, switchAll, switchMap, switchMapTo, tap } from 'rxjs/operators';
 /**
  * Strategies
  * 1. Interleave events by merging streams
@@ -21,6 +21,9 @@ import { map, tap } from 'rxjs/operators';
  *
  * 3. Switch to the latest stream data
  *
+ *  - Suppose you want  a different behavior, such as cancelling the first sequence when a new one begins emitting.
+ *
+ *
  *
 */
 @Component({
@@ -33,15 +36,15 @@ export class OperatorsComponent implements OnInit, OnDestroy {
 
   constructor() {
     console.error('');
-   }
+  }
 
   ngOnInit(): void {
     console.error('Init')
   }
 
   ngOnDestroy(): void {
-      this.destroy$.next();
-      this.destroy$.complete();
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   concatOperator(): void {
@@ -52,11 +55,11 @@ export class OperatorsComponent implements OnInit, OnDestroy {
 
   mergeOperator(): void {
     const source1$ = interval(1000).pipe(
-      map( x => `Source 1:  ${x}`),
+      map(x => `Source 1:  ${x}`),
       take(3)
     );
     const source2$ = interval(1000).pipe(
-      map( y => `Source 2:  ${y}`),
+      map(y => `Source 2:  ${y}`),
       take(3)
     );
     merge(source1$, source2$).subscribe(value => console.error('Merge: ', value));
@@ -64,11 +67,11 @@ export class OperatorsComponent implements OnInit, OnDestroy {
 
   concatIntervalSource(): void {
     const source1$ = interval(1000).pipe(
-      map( x => `Source 1:  ${x}`),
+      map(x => `Source 1:  ${x}`),
       take(3)
     );
     const source2$ = interval(1000).pipe(
-      map( y => `Source 2: ${y}`),
+      map(y => `Source 2: ${y}`),
       take(3)
     );
     concat(source1$, source2$).subscribe(value => console.error('Concat: ', value));
@@ -78,25 +81,35 @@ export class OperatorsComponent implements OnInit, OnDestroy {
     const mouseUp$ = fromEvent(document, 'mouseup');
     const touchEnd$ = fromEvent(document, 'touchend');
     merge(mouseUp$, touchEnd$).pipe(
-      tap(event => console.error(event.type) ),
+      tap(event => console.error(event.type)),
       map((event: any) => {
         let position;
-        switch(event.type) {
+        switch (event.type) {
           case 'touchend':
             position = Object.assign({}, {
               left: event?.changedTouches[0]?.clientX,
               top: event?.changedTouches[0]?.clientY
             });
             break;
-            case 'mouseup':
-              position = Object.assign({}, {
-                left: event?.clientX,
-                top: event?.clientY
-              });
+          case 'mouseup':
+            position = Object.assign({}, {
+              left: event?.clientX,
+              top: event?.clientY
+            });
         }
         return position;
       })
     ).subscribe((obj: any) => console.error('Left: ', obj?.left, 'Top: ', obj?.top))
+  }
+
+  switchBasic(): void {
+    const observable = fromEvent(document, 'click').pipe(
+      map(click => range(1, 3)),
+      switchAll(),
+    ).subscribe(value => console.error('switch all output: ', value));
+
+    merge(fromEvent(document, 'click'), range(1, 3)).subscribe(value => console.error('merge output: ', value))
+
   }
 
 }
